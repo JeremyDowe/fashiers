a8bbd30eeaadf78a8ec52b51583a0e3caa70dbca
JeremyDowe <jeremy.dowe@gmail.com>
2021-12-02 19:02:43 +1000

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 //import jquery from '/bower_components/jquery/dist/jquery.min.js';
 //import zoom from '/bower_components/jquery-zoom/jquery.zoom.js';
-import Glide from '@glidejs/glide';
-import '@glidejs/glide/dist/css/glide.core.css';
-import '@glidejs/glide/dist/css/glide.theme.css';
+//import Glide from '@glidejs/glide';
+//import '@glidejs/glide/dist/css/glide.core.css';
+//import '@glidejs/glide/dist/css/glide.theme.css';
 
 new Glide('.glide', {
 type: 'carousel',
